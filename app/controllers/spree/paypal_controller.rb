cd9f0c75c05e6c86ae62a73d680dde44c145ef68
dandanwei <dandanwei@users.noreply.github.com>
2015-02-28 13:31:09 +0100

    revert the change for noshipping

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -96,7 +96,7 @@ module Spree
           :SolutionType => payment_method.preferred_solution.present? ? payment_method.preferred_solution : "Mark",
           :LandingPage => payment_method.preferred_landing_page.present? ? payment_method.preferred_landing_page : "Billing",
           :cppheaderimage => payment_method.preferred_logourl.present? ? payment_method.preferred_logourl : "",
-          :NoShipping => 0,
+          :NoShipping => 1,
           :PaymentDetails => [payment_details(items)]
       }}
     end
